ea5531ca4012c4f5b9821ceac5dac164994475ef
laxman-boda <laxman.boda96@gmail.com>
2024-06-01 16:03:05 +0530

    Model setup is done

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const express = require('express')
-
-const app = express()
-
-const PORT = 8001;
-
-app.listen(PORT,(req,res) => {
-    console.log(`Connected to Port ${PORT}`)
-})
+function Home(){
+    return (
+        <>
+        </>
+    )
+}
+export default Home
