285c4f676d6d647c53ce6667be6c68bd2e9d46c9
laxman-boda <laxman.boda96@gmail.com>
2024-06-01 15:56:20 +0530

    Model setup is done

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-function Home(){
-    return (
-        <>
-        </>
-    )
-}
-export default Home
+const express = require('express')
+
+const app = express()
+
+const PORT = 8001;
+
+app.listen(PORT,(req,res) => {
+    console.log(`Connected to Port ${PORT}`)
+})
---
+++ models/url.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const urlSchema = new mongoose.Schema({
+    shortId:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    requiredUrl:{
+        type:String,
+        required:true,  
+    },
+    visithistory:[{ timestamp:{type:Number} }]
+},{timestamps:true})
+
+const URL = mongoose.model("url",urlSchema);
+
+module.exports = URL;
